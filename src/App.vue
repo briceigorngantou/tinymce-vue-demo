85a8656cf8f76802b80d86041c1af0479e9524bf
briceigorngantou <briceigor1027@gmail.com>
2023-11-21 09:08:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <editor
+    api-key="no-api-key"
+    :init="{
+      height: 500,
+      menubar: false,
+      plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table paste code help wordcount',
+      ],
+      toolbar:
+        'undo redo | formatselect | bold italic backcolor | \
+           alignleft aligncenter alignright alignjustify | \
+           bullist numlist outdent indent | removeformat | help',
+    }"
+  />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Editor from "@tinymce/tinymce-vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    editor: Editor,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
